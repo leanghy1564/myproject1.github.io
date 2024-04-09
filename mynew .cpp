4dba36444bafbc5f8f68a86cad7f1110b9c314f7
leanghy1564 <165350289+leanghy1564@users.noreply.github.com>
2024-04-09 10:20:47 +0700

    Add files via upload

---
+++ mynew .cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std ;
+int main(){
+	int x=5;
+	cout<<"  x0  =  "  << x;
+	// x += 3;
+	// Cout<<"\n   x1  =  "  << X;
+	// x += 2 ;
+	//cout <<"\n  x2 = "  << x;
+	  x *=3;
+      cout <<  "*=x   => " << x;
+	return 0;
+}
