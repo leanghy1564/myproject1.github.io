3daec957c49e84e2f8cf426ab58fd7f78666b8bf
leanghy1564 <165350289+leanghy1564@users.noreply.github.com>
2024-04-09 10:13:01 +0700

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,14 @@
+ #include<iostream>
+  using namespace std;
+  int main()
+ {
+ 	cout<<"welcome to study C++\n"; 
+ 	cout<<"college CoST";
+ 	cout<<"Computer Science ";
+ 	cout<<"of Technology in the University of Cambodia";
+ 	 
+ 	 return 0; // stop work
+ 	 
+ 	 
+  } 
+ 
